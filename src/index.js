d45a3251003d55ac7b6d3b5040ca61bde92dcdbf
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-01-15 18:24:25 +0100

    Test new methods

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
-const User = require ('./user');
-const Recipe = require ('./recipe');
-const Ingredient = require ('./ingredient');
+const User = require('./user')
+const Recipe = require('./recipe')
+const Ingredient = require('./ingredient')
 
-const Brownie = new Recipe ('Brownie', 'easy', '30 min', 4);
-console.log(Brownie);
+const Brownie = new Recipe('Brownie', 'easy', '30 min', 4)
+const Maria = new User('Maria', 'maria@gmail.com', 'potato')
+const chocolate = new Ingredient('chocolate', 200, 'grams')
+Maria.likeRecipe(Brownie)
+Brownie.addIngredients(chocolate)
+Brownie.addDiet('Vegetarian, GF')
+console.log(Maria)
+console.log(Brownie)
 
-const Maria = new User ('Maria', 'maria@gmail.com', 'potato');
-console.log(Maria);
-
-const flour = new Ingredient('flour', 500, 'grams');
-console.log(flour.amount);
-console.log(flour.toPounds());
+const flour = new Ingredient('flour', 500, 'grams', 'grams')
+console.log(flour.amount)
+console.log(flour.changeMeasurement())
