7c8d7ea7ec06cd94ddacb2e1468209beed0e9089
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-01-15 19:22:56 +0100

    Fix spacing between methods

--- src/ingredient.js
+++ src/ingredient.js
@@ -1,24 +1,23 @@
 class Ingredient {
-    constructor (name, amount, unit) {
-        this.name = name
-        this.amount = amount
-        this.unit = unit
+  constructor(name, amount, unit) {
+    this.name = name
+    this.amount = amount
+    this.unit = unit
+  }
+
+  changeMeasurement() {
+    if (this.unit == 'grams') {
+      const num = this.amount * 0.0022046
+      if (num > 1) {
+        const lbs = num.toPrecision(1)
+        const oz = (num - lbs * 16).toFixed(1)
+        return `${lbs}lb ${oz}oz`
+      }
+      const ounces = (this.amount / 28.34952).toPrecision(2)
+      return `${ounces}oz`
     }
-    changeMeasurement() {
-        if (this.unit == 'grams') {
-            let num = this.amount * 0.0022046;
-            if (num > 1) {
-            let lbs = num.toPrecision(1);
-            let oz = (num-(lbs*16)).toFixed(1); 
-            return `${lbs}lb ${oz}oz`;
-            } else {
-            let ounces = (this.amount/28.34952).toPrecision(2);
-            return `${ounces}oz`;
-            }
-        } else {
-            throw new Error ("Sorry, this unit of measurement can not be converted.");
-        }
-    } 
+    throw new Error('Sorry, this unit of measurement can not be converted.')
+  }
 }
 
 module.exports = Ingredient
