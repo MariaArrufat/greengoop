a4a6f8f04a456bc81ed920bd7b4a5df3d060e92f
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-01-19 11:01:17 +0100

    Create Comments class

---
+++ src/comments.js
@@ -0,0 +1,25 @@
+const now = new Date()
+let month = now.getMonth() + 1
+if (month < 9) {
+  month = `0${month}`
+}
+let day = now.getDate()
+if (day < 9) {
+  day = `0${day}`
+}
+
+class Comments {
+  constructor(recipe, user, comment) {
+    this.recipe = recipe
+    this.user = user
+    this.date = `${day}/${month}/${now.getFullYear()}`
+    this.comment = comment
+    this.myPic = []
+  }
+
+  addPic(pic) {
+    this.myPic.push(pic)
+  }
+}
+
+module.exports = Comments
