5feeaa64ee55cc204bc9855ee1a02a7d8826ce7f
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-01-19 22:49:06 +0100

    Add tests and sort most liked recipes

--- src/index.js
+++ src/index.js
@@ -1,16 +1,39 @@
 const User = require('./user')
 const Recipe = require('./recipe')
 const Ingredient = require('./ingredient')
+const Comments = require('./comments');
 
-const Brownie = new Recipe('Brownie', 'easy', '30 min', 4)
+const Brownie = new Recipe('Brownie', 'easy', '30 min', 1)
+const Salad = new Recipe('Salad', 'Easy', '10 min', 2)
+const Soup = new Recipe ('Soup', 'Easy', '40 min', 4)
+
 const Maria = new User('Maria', 'maria@gmail.com', 'potato')
+const Tyson = new User ('Tyson', 'tysonlaa@gmail.com', 'averyhardpssw')
+const Teresa = new User ('Tere', 'terew@hotmail.com', 'ilikepink')
 const chocolate = new Ingredient('chocolate', 200, 'grams')
-Maria.likeRecipe(Brownie)
-Brownie.addIngredients(chocolate)
-Brownie.addDiet('Vegetarian, GF')
-console.log(Maria)
-console.log(Brownie)
-
 const flour = new Ingredient('flour', 500, 'grams', 'grams')
-console.log(flour.amount)
-console.log(flour.changeMeasurement())
+const firstComment = new Comments (Brownie, Maria, 'These look yummy!')
+
+Maria.likeRecipe(Brownie)
+Maria.likeRecipe(Salad)
+Tyson.likeRecipe(Salad)
+Maria.unlikeRecipe(Salad)
+Teresa.likeRecipe(Salad)
+Teresa.likeRecipe(Soup)
+
+Brownie.addIngredients(chocolate, flour)
+Brownie.addDiet('Vegetarian, GF')
+
+/* console.log(Brownie)
+console.log(Salad)
+console.log(firstComment) */
+
+Brownie.showDetail();
+Brownie.adjustServings(2);
+console.log(Brownie)
+Brownie.showDetail();
+
+// Most liked recipes: 
+const allRecipes = [Brownie, Salad, Soup]
+allRecipes.sort((a, b) => (b.likes > a.likes) ? 1 : -1);
+allRecipes.forEach((r) => {console.log(r.name);});
