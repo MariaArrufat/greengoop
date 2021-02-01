12a0a7a925229c78015555a24fb3feafda5f7d89
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-02-01 16:45:02 +0100

    Create users and recipe routes

---
+++ routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var users = [
+  { name: 'maria', age:25 },
+  { name: 'tyson', age:27 }
+]
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  if (req.query.name) {
+    return res.send(users.find(user => user.name == req.query.name))
+  }
+  res.send(users);
+});
+
+router.get('/:userId', function(req, res, next) {
+  const user = users[req.params.userId]
+  if (user) res.send(user)
+  else res.sendStatus(404)
+})
+
+module.exports = router;
