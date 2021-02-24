e6d4a47a1403091d8e47613e86c19e2cf18372dd
Maria Arrufat <maria.arrufat.recasens@gmail.com>
2021-02-24 11:59:38 +0100

    Reorganized folder routes

---
+++ src/routes/recipes.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+var recipes = [
+    {name: 'brownie', difficulty: 'easy'}
+]
+
+/* GET recipes listing. */
+router.get('/', function(req, res, next) {
+  if (req.query.name) {
+    return res.send(recipes.find(recipe => recipe.name == req.query.name))
+  }
+  res.send(recipes);
+});
+
+/* GET specific recipe. */
+router.get('/:recipeId', function(req, res, next) {
+  const recipe = recipes[req.params.recipeId]
+  if (recipe) res.send(recipe)
+  else res.sendStatus(404) // else next(new Error('User not found'))
+})
+
+/* GET recipes filtered by diet - GF. */
+router.get('/recipes/:diet', function(req, res, next) {
+    const recipe = recipes[req.params.recipeId]
+    if (recipe) res.send(recipe)
+    else res.sendStatus(404) // else next(new Error('User not found'))
+  })
+
+module.exports = router;
